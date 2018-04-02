0801e64f9f90921308442a949043b90823d83003
Esther <esther.berges@hotmail.com>
2018-04-03 00:40:20 +0200

    Animation du titre
    anim

---
+++ Assets/Scripts/DoRandomScale.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class DoRandomScale : MonoBehaviour {
+
+    public float min = 5;
+    public float max = 40;
+
+    // Use this for initialization
+    void Start ()
+    {
+        DoRandomStart();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void DoRandomStart()
+    {
+        float i = Random.Range(min, max);
+
+        this.transform.DOScaleY(i, Random.Range(0.15f, 1f)).SetEase(Ease.OutBack).OnComplete(DoRandomStart);
+    }
+}
